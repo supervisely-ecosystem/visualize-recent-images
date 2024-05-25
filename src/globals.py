39454c5a53115d1597cb3ef9cb136cb02ccd9bbe
vorozhkog <vorozkhog@icloud.com>
2024-05-25 20:52:27 +0100

    update code to review comments

--- src/globals.py
+++ src/globals.py
@@ -3,9 +3,6 @@ import os
 import supervisely as sly
 from dotenv import load_dotenv
 
-from supervisely.app.widgets import (
-    GridGallery,
-)
 
 if sly.is_development():
     load_dotenv("local.env")
@@ -23,13 +20,7 @@ selected_dataset = sly.env.dataset_id(raise_not_found=False)
 delay = int(os.environ.get("modal.state.UpdateDelay", 2))
 col_num = int(os.environ.get("modal.state.GridWidth", 2))
 
-project_info = api.project.get_info_by_id(selected_project)
+project_info = api.project.get_info_by_id(selected_project) if selected_project else None
 
 last_time = project_info.updated_at
 current_time = None
-
-grid = GridGallery(
-    col_num,
-)
-
-continue_working = True
--- src/ui/input.py
+++ src/ui/input.py
@@ -6,16 +6,21 @@ from supervisely.app.widgets import (
 
 import src.globals as g
 from datetime import datetime
+
 from supervisely._utils import is_production
+from supervisely.app.widgets import GridGallery
+
+grid = GridGallery(
+    g.col_num,
+)
 
 
 def get_latest_imgs(imginfos):
-    infos_w_dates = [(image, datetime.fromisoformat(image.updated_at[:-1])) for image in imginfos]
-
-    sorted_infos_with_dates = sorted(infos_w_dates, key=lambda x: x[1], reverse=True)
-    sorted_infos = [image[0] for image in sorted_infos_with_dates]
-
-    return sorted_infos[: g.col_num]
+    return sorted(
+        imginfos,
+        key=lambda img_info: datetime.fromisoformat(img_info.updated_at[:-1]),
+        reverse=True,
+    )[: g.col_num]
 
 
 def update_grid():
@@ -43,21 +48,21 @@ def update_grid():
             preview_urls.append(
                 img.preview_url.replace("http://10.62.10.5:32977/", "https://dev.supervisely.com/")
             )
-    image_names = [img.name for img in img_infos]
+    img_names = [img.name for img in img_infos]
     ann_jsons = [g.api.annotation.download(img_id) for img_id in img_ids]
+
     project_meta = sly.ProjectMeta.from_json(g.api.project.get_meta(g.selected_project))
     anns = [sly.Annotation.from_json(ann_json.annotation, project_meta) for ann_json in ann_jsons]
     if len(anns) == 0:
         anns = [None]
-    g.grid.clean_up()
-    # sly.logger.info("Grid Cleaned")
-    for url, name, ann in zip(preview_urls, image_names, anns):
-        g.grid.append(
+
+    grid.clean_up()
+    for url, name, ann in zip(preview_urls, img_names, anns):
+        grid.append(
             title=name,
             image_url=url,
             annotation=ann,
         )
-    # sly.logger.info("Grid images added")
 
 
 update_grid()
@@ -66,7 +71,7 @@ card = Card(
     "The most recently updated images",
     content=Container(
         widgets=[
-            g.grid,
+            grid,
         ]
     ),
 )
